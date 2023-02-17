d2949e8d05dfe5e4dd0c6412dae5d962c1970339
Luckyjnr <luckykelimu@gmail.com>
2023-02-17 03:44:10 -0800

    my task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for (i = 'a' ;  i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n")
+	return (0);
+}
