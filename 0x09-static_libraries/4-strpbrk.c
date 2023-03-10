6bc2f9513fc89f52c08fcd6eaed2af42b08754ad
Luckyjnr <luckykelimu@gmail.com>
2023-03-10 13:51:30 -0800

    my task 0

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,25 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _strpbrk - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ * _strpbrk -  a function that searches a string for any of a set of bytes.
+ * @s: an input string
+ * @accept: an input character with to locate into string s
+ * Return:  a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int k;
-
+	char *start = accept;
 
 	while (*s)
 	{
-		for (k = 0; accept[k]; k++)
+		while (*accept)
 		{
-		if (*s == accept[k])
-		return (s);
+			if (*accept == *s)
+				return (s);
+			accept++;
 		}
-	s++;
+
+		accept = start;
+		s++;
 	}
-
-
-return ('\0');
+	return (NULL);
 }
