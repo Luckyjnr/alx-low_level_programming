45ef5913c844671325ed81d987794bf81c823fef
Luckyjnr <luckykelimu@gmail.com>
2023-03-20 07:26:58 -0700

    my task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -69,5 +69,26 @@ int _strlen(char *s)
 	return (c);
 }
 
+/**
+  * _strcpy - Copy a string
+  * @dest: Destination value
+  * @src: Source value
+  *
+  * Return: the pointer to dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
+
 
 
