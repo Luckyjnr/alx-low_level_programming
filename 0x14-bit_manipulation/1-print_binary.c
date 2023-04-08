81439625bf0fff01a2eb52e1f55f569ebd348e28
Luckyjnr <luckykelimu@gmail.com>
2023-04-07 22:48:27 -0700

    my task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,29 +1,20 @@
 #include "main.h"
 
 /**
- * print_binary - Prints the binary representation
- * of a number
+ * print_binary - Prints a number with or without % or /
  * @n: unsigned long int.
  * Return: Nothing
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
-
-	if (n == 0)
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
 	{
 		_putchar('0');
-		return;
-	}
-	while (max)
-	{
-		if (flag == 1 && (n & max) == 0)
-			_putchar('0');
-		else if ((n & max) != 0)
-		{
-			_putchar('1');
-			flag = 1;
-		}
-		max >>= 1;
 	}
 }
