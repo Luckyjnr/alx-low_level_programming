fcf5874fff36fe9a727267affd28618c4d9e3d6c
Luckyjnr <luckykelimu@gmail.com>
2023-04-09 21:33:09 -0700

    my task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,32 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * create_file - Creates a file
  * @filename: Filename
- * @text_content: String to write to the file
- * Return: 1 if it is a success and -1 if it fails
+ * @text_content: String in the file
+ * Return: 1 if successful, else -1
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fdo;
-	int fdw;
-	int len;
+	int fd, rwr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fdo = open(filename, O_CREAT | O_CREAT | O_TRUNC, 0600);
-	if (fdo == -1)
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
+	while (text_content && *(text_content + len))
+		len++;
 
-	for (fdw = 0; text_content[fdw]; fdw++)
-		;
-	len = write(fdo, text_content, fdw);
-	if (len == -1)
+	rwr = write(fd, text_content, len);
+	close(fd);
+	if (rwr < 0)
 		return (-1);
-	close(fdo);
-
 	return (1);
 }
