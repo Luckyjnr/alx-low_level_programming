dfdfcac61e96b461bdfd1149a2844a13b0f36590
Luckyjnr <luckykelimu@gmail.com>
2023-04-09 21:28:28 -0700

    my task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,23 +10,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fdo,
-	int fdw,
-	int len = 0
+	int fdo;
+	int fdw;
+	int len;
 
 	if (filename == NULL)
 		return (-1);
 
-	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fdo < 0)
+	fdo = open(filename, O_CREAT | O_CREAT | O_TRUNC, 0600);
+	if (fdo == -1)
 		return (-1);
 
-	while (text_content && *(text_content + len))
-		len++;
+	if (!text_content)
+		text_content = "";
 
-	fdw = write(fdo, text_content, len);
+	for (fdw = 0; text_content[fdw]; fdw++)
+		;
+	len = write(fdo, text_content, fdw);
+	if (len == -1)
+		return (-1);
 	close(fdo);
-	if (fdw < 0)
-		return (-1);
+
 	return (1);
 }
